b9ab1d80cb1296f4ef2198add84a2ea8ca78f189
TH3 S4LM0N <blakeblankenship@ccawarriors.com>
2022-12-26 04:37:32 +0000

    maybe work now

--- target/debug/deps/lib_salmon-aaa196cf95e6e935.d
+++
@@ -1,5 +0,0 @@
-/home/salmon/projects/rust/lib-salmon/Rust/target/debug/deps/lib_salmon-aaa196cf95e6e935.rmeta: src/lib.rs
-
-/home/salmon/projects/rust/lib-salmon/Rust/target/debug/deps/lib_salmon-aaa196cf95e6e935.d: src/lib.rs
-
-src/lib.rs:
--- target/debug/deps/lib_salmon-ad2b0e53b8043f0d.d
+++
@@ -1,5 +0,0 @@
-/home/salmon/projects/rust/lib-salmon/Rust/target/debug/deps/lib_salmon-ad2b0e53b8043f0d.rmeta: src/lib.rs
-
-/home/salmon/projects/rust/lib-salmon/Rust/target/debug/deps/lib_salmon-ad2b0e53b8043f0d.d: src/lib.rs
-
-src/lib.rs:
